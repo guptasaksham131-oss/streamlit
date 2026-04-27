e64cecb8a2944498e15cb70176b8e253ef21ddc3
guptasaksham131-oss <guptasaksham131@gmail.com>
2026-04-27 10:27:23 +0530

    Add files via upload

---
+++ import streamlit.py
@@ -0,0 +1,19 @@
+import streamlit as st
+#TITLE OF APP
+st.title("my first streamlit app")
+#adding text
+st.write("hello! creating a simple web applications using streamlit library.")
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+ 
